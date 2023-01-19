de226480cd0ced9965ade99ac53c0bc93a098054
nikolalabovic <nikolalabovicpn@gmail.com>
2023-01-19 14:46:06 +0100

    add route table & default route

--- main.tf
+++ main.tf
@@ -25,3 +25,17 @@ resource "aws_internet_gateway" "nas_internet_gateway" {
     Name = "dev-igw"
   }
 }
+
+resource "aws_route_table" "nasa_public_rt" {
+  vpc_id = aws_vpc.nas_vpc.id
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.nasa_public_rt.id
+  destination_cidr_block = "0.0.0.0/0" #sve ip adrese ce ici na internet gateway, koji definisemo dole
+  gateway_id = aws_internet_gateway.nas_internet_gateway.id
+}
