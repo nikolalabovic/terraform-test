943371f0da4536fe9dbf61bd403c81756c5682e2
nikolalabovic <nikolalabovicpn@gmail.com>
2023-01-19 14:13:48 +0100

    add igw and format

--- main.tf
+++ main.tf
@@ -1,19 +1,27 @@
 resource "aws_vpc" "nas_vpc" {
-  cidr_block = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
-      Name="dev"
+    Name = "dev"
   }
 }
 
 resource "aws_subnet" "nas_public_subnet" {
-  vpc_id = aws_vpc.nas_vpc.id
-  cidr_block = "10.123.1.0/24"
-  availability_zone = "eu-central-1a"
+  vpc_id                  = aws_vpc.nas_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name="dev-public"
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "nas_internet_gateway" {
+  vpc_id = aws_vpc.nas_vpc.id
+
+  tags = {
+    Name = "dev-igw"
   }
 }
