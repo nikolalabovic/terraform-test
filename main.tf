abf8a12cc7077033d8cf3f340fb523714f7b3ebf
nikolalabovic <nikolalabovicpn@gmail.com>
2023-02-03 15:27:24 +0100

    add all ip addr

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "nas_sg" {
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
-    cidr_blocks = ["188.120.99.252/32"]
+    cidr_blocks = ["188.120.99.252/32","0.0.0.0/0"]
   }
 
   egress {
